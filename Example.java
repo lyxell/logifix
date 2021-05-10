8268348139de2c64dcb09b0595751de953bd09a4
lyxell <alyxell@kth.se>
2021-05-10 13:54:34 +0200

    Remove Example.java

--- Example.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-import java.util.ArrayList;
-
-public class Example {
-    public static void main(String[] args) {
-        List<Integer> x = new ArrayList<Integer>();
-        if (x.size() == 0) {}
-    }
-}
