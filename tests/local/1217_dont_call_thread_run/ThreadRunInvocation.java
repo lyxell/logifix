7fc4335b95b5c166844eb1d60552ddb9a6d3ae95
lyxell <alyxell@kth.se>
2021-06-08 16:10:39 +0200

    Add test for rule 1217

---
+++ tests/local/1217_dont_call_thread_run/ThreadRunInvocation.java
@@ -0,0 +1,15 @@
+class ThreadRunInvocation {
+  public static void main(String[] args) {
+    Runnable runnable = null;
+
+    Thread myThread = new Thread(runnable);
+    myThread.run(); // Noncompliant
+
+    Thread myThread2 = new Thread(runnable);
+    myThread2.start();
+
+    run();
+  }
+
+  public static void run() {}
+}
--- tests/local/run_tests.sh
+++ tests/local/run_tests.sh
@@ -7,5 +7,5 @@ logifix_cli="$1"
 
 for f in $(find $BASEDIR -iname '*.java'); do
     echo "$f"
-    diff <("$1" --rules=1125,1155,4973 --no-apply --no-interactive --no-color "$f" | tail -n +2) <(tail -n +2 $f.diff)
+    diff <("$1" --rules=1125,1155,1217,4973 --no-apply --no-interactive --no-color "$f" | tail -n +2) <(tail -n +2 $f.diff)
 done
