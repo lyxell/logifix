78b81df3134d3d7253d1e4923757067fc9f827cd
lyxell <alyxell@kth.se>
2021-06-28 23:34:31 +0200

    Add rule 1126

---
+++ tests/local/1126_simplify_boolean_return/Test.java
@@ -0,0 +1,28 @@
+class Test {
+    boolean test1() {
+        int x = 20;
+        if (x == 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    boolean test2() {
+        int x = 20;
+        if (x == 20) return true;
+        else return false;
+    }
+    boolean test3() {
+        int x = 20;
+        if (x == 20) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    boolean test4() {
+        int x = 20;
+        if (x == 20) return false;
+        else return true;
+    }
+}
