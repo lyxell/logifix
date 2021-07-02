e071e36fa16a6958f4c552811dc1de4702cc62ea
lyxell <alyxell@kth.se>
2021-07-02 03:02:20 +0200

    Update cli flags for tests

--- tests/real_world/run_tests.sh
+++ tests/real_world/run_tests.sh
@@ -17,7 +17,7 @@ tail +2 < "$csv" | while IFS= read -r f; do
         echo "$rule_number"
         cp "$input" "$t1"
         # run the logifix tool on t1
-        "$logifix_cli" --no-interactive --rules="$rule_number" "$t1"
+        "$logifix_cli" --in-place --accept="$rule_number" "$t1"
         # run the patch tool and place the result in t2
         patch -p0 --input="$input.diff" --output="$t2"
         # compare the diffs
