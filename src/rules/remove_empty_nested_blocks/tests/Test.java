141259e7e5296c33568d0910a24abaf1957348f2
lyxell <alyxell@kth.se>
2021-07-08 16:11:54 +0200

    feat: Improve detection for remove_empty_nested_blocks
    Now handles empty blocks in else branch of if-statement

---
+++ src/rules/remove_empty_nested_blocks/tests/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println("hello");
+        } else {} // remove
+
+        {} // remove
+    }
+}
