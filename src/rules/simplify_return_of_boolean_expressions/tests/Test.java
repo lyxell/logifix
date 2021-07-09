752248dc7d650e0809f667ec6b32d9205a3f313d
lyxell <alyxell@kth.se>
2021-07-09 02:11:26 +0200

    test: Add more tests to simplify_return_of_boolean_expressions

--- src/rules/simplify_return_of_boolean_expressions/tests/Test.java
+++ src/rules/simplify_return_of_boolean_expressions/tests/Test.java
@@ -1,28 +1,39 @@
 class Test {
-    boolean test1() {
-        int x = 20;
+    boolean test1(int x) {
         if (x == 20) {
             return true;
         } else {
             return false;
         }
     }
-    boolean test2() {
-        int x = 20;
+    boolean test2(int x) {
         if (x == 20) return true;
         else return false;
     }
-    boolean test3() {
-        int x = 20;
+    boolean test3(int x) {
         if (x == 20) {
             return false;
         } else {
             return true;
         }
     }
-    boolean test4() {
-        int x = 20;
+    boolean test4(int x) {
         if (x == 20) return false;
         else return true;
+    }
+    boolean test5(int x) {
+        if (x == 20) return false;
+        else return true;
+    }
+    boolean test6(int x) {
+        if (x == 20) return ( false );
+        else return (  true  );
+    }
+
+    boolean test6(int x) {
+        if    (x == 20)
+                return  ( false );
+        else
+                return   ( ( (true) ) );
     }
 }
