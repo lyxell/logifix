e7ea7a0bb40464371b9e2f72be10db05445914b7
lyxell <alyxell@kth.se>
2021-07-09 02:17:46 +0200

    test: Add more tests to remove_unused_imports

---
+++ src/rules/remove_unused_imports/tests/TestOnDemand.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println(list.size());
+    }
+}
