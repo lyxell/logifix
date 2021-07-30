4f27f804ece337fe13fd9d7909d5f047510e12e6
lyxell <alyxell@kth.se>
2021-07-30 02:11:02 +0200

    refactor: Remove unused declarations

--- src/logifix.h
+++ src/logifix.h
@@ -16,13 +16,9 @@ node_id add_file(const std::string& file);
 
 void run(std::function<void(node_id)> report_progress);
 
-bool edit_scripts_are_equal(std::string_view o, std::string_view a,
-                            std::string_view b, std::string_view c);
-
 std::set<std::pair<rule_id, std::string>> get_patches(const std::string&);
 
-std::vector<std::pair<rule_id, std::string>>
-get_patches_for_file(node_id file);
+std::vector<std::pair<rule_id, std::string>> get_patches_for_file(node_id file);
 
 void print_performance_metrics();
 
