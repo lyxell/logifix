37390e7ac0a9bc3ef669a336384a7f5811a66b60
lyxell <alyxell@kth.se>
2021-08-14 03:13:11 +0200

    Refactor type system

--- src/functors.cpp
+++ src/functors.cpp
@@ -18,4 +18,54 @@ souffle::RamDomain decrease_indentation(souffle::SymbolTable* symbolTable,
     }
     return symbolTable->encode(result);
 }
+
+souffle::RamDomain type_to_string(souffle::SymbolTable* symbolTable,
+                                  souffle::RecordTable* recordTable, souffle::RamDomain arg) {
+    const souffle::RamDomain* tuple = recordTable->unpack(arg, 3);
+    const std::string& cl = symbolTable->decode(tuple[1]);
+    std::vector<std::string> type_args;
+    auto curr = tuple[2];
+    while (curr != 0) {
+        const auto* pair = recordTable->unpack(curr, 2);
+        type_args.emplace_back(
+            symbolTable->decode(type_to_string(symbolTable, recordTable, pair[0])));
+        curr = pair[1];
+    }
+    std::string result = cl;
+    if (!type_args.empty()) {
+        result += "<";
+        for (const auto& arg : type_args) {
+            result += arg + ",";
+        }
+        result.pop_back();
+        result += ">";
+    }
+    return symbolTable->encode(result);
+}
+
+souffle::RamDomain type_to_qualified_string(souffle::SymbolTable* symbolTable,
+                                            souffle::RecordTable* recordTable,
+                                            souffle::RamDomain arg) {
+    const souffle::RamDomain* tuple = recordTable->unpack(arg, 3);
+    const std::string& package = symbolTable->decode(tuple[0]);
+    const std::string& cl = symbolTable->decode(tuple[1]);
+    std::vector<std::string> type_args;
+    auto curr = tuple[2];
+    while (curr != 0) {
+        const auto* pair = recordTable->unpack(curr, 2);
+        type_args.emplace_back(
+            symbolTable->decode(type_to_qualified_string(symbolTable, recordTable, pair[0])));
+        curr = pair[1];
+    }
+    std::string result = package + "." + cl;
+    if (!type_args.empty()) {
+        result += "<";
+        for (const auto& arg : type_args) {
+            result += arg + ",";
+        }
+        result.pop_back();
+        result += ">";
+    }
+    return symbolTable->encode(result);
+}
 }
