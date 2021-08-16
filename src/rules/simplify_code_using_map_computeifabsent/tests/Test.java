16a56c2483bea25011dd470c96cb3bb596902954
lyxell <alyxell@kth.se>
2021-08-16 22:59:34 +0200

    Add more regression tests

--- src/rules/simplify_code_using_map_computeifabsent/tests/Test.java
+++ src/rules/simplify_code_using_map_computeifabsent/tests/Test.java
@@ -1,10 +1,12 @@
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Test {
 
+    Map<Class<?>, String> concurrentMap = new ConcurrentHashMap<>();
     Map<String, Integer> myMap;
 
-    Integer test(String key) {
+    Integer test1(String key) {
         Integer result = myMap.get(key);
         if (result == null) {
             result = key.length() + 10;
@@ -13,8 +15,17 @@ public class Test {
         return result;
     }
 
+    public String test2(Class<?> x, String name) {
+        String str = concurrentMap.get(x);
+        if (str == null) {
+            str = "{" + name + ":" + x.getName() + "}";
+            concurrentMap.put(x, str);
+        }
+        return str;
+    }
+
     public static void main(String[] args) {
         Test test = new Test();
-        System.out.println(test.test("hello"));
+        System.out.println(test.test1("hello"));
     }
 }
