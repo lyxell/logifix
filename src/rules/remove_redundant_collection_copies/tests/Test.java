a69d208acdb37c21a4bac4da92a2c15db82b9f83
lyxell <alyxell@kth.se>
2021-08-17 01:26:55 +0200

    Add more regression tests

--- src/rules/remove_redundant_collection_copies/tests/Test.java
+++ src/rules/remove_redundant_collection_copies/tests/Test.java
@@ -1,9 +1,13 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 class Test {
     List<Integer> test(List<Integer> original) {
-        return new ArrayList<>(original.stream().filter(x > 0).collect(Collectors.toList()));
+        return new ArrayList<>(original.stream().filter(x -> x > 0).collect(Collectors.toList()));
+    }
+    List<Integer> testAsList(List<Integer> original) {
+        return new ArrayList<>(Arrays.asList(1,2,3));
     }
 }
