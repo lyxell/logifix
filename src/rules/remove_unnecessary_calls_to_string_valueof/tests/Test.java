84ff64b6dd90d75a10138933f07ae5715b013bc6
lyxell <alyxell@kth.se>
2021-08-19 15:22:13 +0200

    Add typechecking for Map.Entry type args

--- src/rules/remove_unnecessary_calls_to_string_valueof/tests/Test.java
+++ src/rules/remove_unnecessary_calls_to_string_valueof/tests/Test.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+
 class Test {
     void test1() {
         String s = "hello ";
@@ -20,5 +22,9 @@ class Test {
     void test4(String s) {
         System.out.println(String.valueOf(s));
         System.out.println(String.valueOf("hello"));
+    }
+    void test5(Map.Entry<String,String> s) {
+        System.out.println(String.valueOf(s.getKey()));
+        System.out.println(String.valueOf(s.getValue()));
     }
 }
