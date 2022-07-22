96cc979132894f8e1da3eec7cfefd625a3b954ac
Anton Lyxell <alyxell@kth.se>
2022-07-22 22:12:17 +0200

    feat: Improve type checking of streams

--- src/rules/remove_redundant_collection_copies/tests/Test.java
+++ src/rules/remove_redundant_collection_copies/tests/Test.java
@@ -5,13 +5,13 @@ import java.util.stream.Collectors;
 
 class Test {
     List<Integer> test(List<Integer> original) {
-        return new ArrayList<>(original.stream().filter(x -> x > 0).collect(Collectors.toList()));
+        return new ArrayList<>(original.stream().map(x -> x + 2).filter(x -> x > 0).collect(Collectors.toList()));
     }
     List<Integer> testAsList(List<Integer> original) {
         return new ArrayList<>(Arrays.asList(1,2,3));
     }
     void testAssignment(List<Integer> original) {
-        List<Integer> list = new ArrayList<>(original.stream().filter(x -> x > 0).collect(Collectors.toList()));
+        List<Integer> list = new ArrayList<>(original.stream().map(x -> x + 3).filter(x -> x > 0).collect(Collectors.toList()));
         System.out.println(list.size());
     }
     void testAsListAssignment(List<Integer> original) {
